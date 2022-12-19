6b62944fd7ba5479b96293a3f2e3ce29f1041384
florent sandri <sandri822@hotmail.fr>
2022-12-19 16:20:07 +0100

    ✅ Update content route letter

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class LettersController < ApplicationController
+  require 'baby_name_generator'
+  include BabyNameGenerator
+
   def show
     @letter = params[:letter]
+    @baby_names, @name_count = BabyNameGenerator.generate_baby_names
+
+    @baby_names = @baby_names.select { |name| name.start_with?(@letter) }
+    @name_count = @name_count.select { |name| name.start_with?(@letter) }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/new'
-
   get 'letters/:letter', to: 'letters#show', as: 'letter'
 
   # Defines the root path route ("/")
