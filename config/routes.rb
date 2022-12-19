07d09cc1d033e1775e5dae2593875e7a824bc7a1
Lauris Bessone <laurisbessonepro@gmail.com>
2022-12-19 16:33:45 +0100

    :lipstick: link to buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'letters/:letter', to: 'letters#show', as: 'letter'
-
+  get 'home/new'
   # Defines the root path route ("/")
-  root "home#description"
+  root 'home#description'
 end
