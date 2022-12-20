eba64e60c440fbf1604276fd3ac02fa085c96848
Lauris Bessone <laurisbessonepro@gmail.com>
2022-12-20 15:12:13 +0100

    🔥 supression fichier inutiles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,5 @@ class HomeController < ApplicationController
   require 'baby_name_generator'
   include BabyNameGenerator
 
-  def new
-    @baby_names, @name_count = BabyNameGenerator.generate_baby_names
-  end
-
   def description; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'letters/:letter', to: 'letters#show', as: 'letter'
   get 'baby_names', to: 'baby_names#baby_names'
   get 'home/new'
   # Defines the root path route ("/")
