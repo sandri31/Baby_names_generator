99f84d3fc72a8795287dd979ac1b5040180485c7
florent sandri <sandri822@hotmail.fr>
2022-12-21 07:36:05 +0100

    🐛 Fix a bug

--- app/controllers/baby_names_controller.rb
+++ app/controllers/baby_names_controller.rb
@@ -5,8 +5,8 @@ class BabyNamesController < ApplicationController
   include BabyNameGenerator
 
   def baby_names
-    starts_with = params[:starts_with]
-    ends_with = params[:ends_with]
+    starts_with = params[:starts_with].downcase
+    ends_with = params[:ends_with].downcase
 
     @names = BabyNameGenerator.generate_baby_generator(starts_with: starts_with, ends_with: ends_with).to_a
   end
