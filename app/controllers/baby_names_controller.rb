90e8bf6947a1c588c2db08e0007ad8a68bc713fa
florent sandri <sandri822@hotmail.fr>
2022-12-21 21:33:37 +0100

    🚧 Work in progress

--- app/controllers/baby_names_controller.rb
+++ app/controllers/baby_names_controller.rb
@@ -10,4 +10,9 @@ class BabyNamesController < ApplicationController
 
     @names = BabyNameGenerator.generate_baby_generator(starts_with: starts_with, ends_with: ends_with).to_a
   end
+
+  def update_required_chars
+    BabyNameGenerator.required_global = params[:required_chars].split(' ')
+    redirect_to home_path
+  end
 end
