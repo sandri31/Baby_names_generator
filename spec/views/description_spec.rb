2c3e162576cb23c774f78d2e50f3b0bccf0b02e5
Symitsh <sandri822@hotmail.fr>
2023-01-03 16:44:48 +0100

    ✅ Add test with rspec

--- spec/views/description_spec.rb
+++ spec/views/description_spec.rb
@@ -3,35 +3,87 @@
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.describe 'home/description.html.erb', type: :view do
-  describe 'Baby name form' do
-    include Capybara::DSL
+describe 'Baby name form_with' do
+  include Capybara::DSL
 
-    it 'renders the form' do
-      visit root_path
-      expect(page).to have_selector 'form'
-    end
+  it 'renders the form' do
+    visit root_path
+    expect(page).to have_selector 'form'
+  end
 
-    it 'renders the form fields' do
-      visit root_path
-      expect(page).to have_field 'starts_with'
-      expect(page).to have_field 'ends_with'
-      expect(page).to have_selector 'label', text: 'Lettre(s) de début :'
-      expect(page).to have_selector 'label', text: 'Lettre(s) de fin :'
-    end
+  it 'renders the form fields' do
+    visit root_path
+    expect(page).to have_field 'starts_with'
+    expect(page).to have_field 'ends_with'
+    expect(page).to have_selector 'label', text: 'Lettre(s) de début :'
+    expect(page).to have_selector 'label', text: 'Lettre(s) de fin :'
+  end
 
-    it 'renders the submit button' do
-      visit root_path
-      expect(page).to have_button 'Générer'
-    end
+  it 'renders the submit button' do
+    visit root_path
+    expect(page).to have_button 'Générer'
+  end
 
-    it 'redirects to the baby names page' do
-      visit root_path
-      fill_in 'starts_with', with: 'A'
-      fill_in 'ends_with', with: '2'
-      click_button 'Générer'
-      expect(page).to have_current_path '/baby_names?starts_with=A&ends_with=2&commit=Générer', wait: 10
-      expect(page).to have_content 'A'
-    end
+  it 'Check that the fields starts_with and ends_with exist' do
+    visit root_path
+    fill_in 'starts_with', with: 'A'
+    fill_in 'ends_with', with: 'A'
+    click_button 'Générer'
+  end
+
+  it 'Check number in starts_with' do
+    visit root_path
+    fill_in 'starts_with', with: '12'
+    click_button 'Générer'
+    expect(page).to have_current_path('/baby_names?starts_with=12&ends_with=&commit=Générer')
+  end
+
+  it 'Check number in ends_with' do
+    visit root_path
+    fill_in 'ends_with', with: '8'
+    click_button 'Générer'
+    expect(page).to have_current_path('/baby_names?starts_with=&ends_with=8&commit=Générer')
+  end
+
+  it 'Check path with starts_with' do
+    visit root_path
+    fill_in 'starts_with', with: 'A'
+    fill_in 'ends_with', with: ''
+    click_button 'Générer'
+    expect(page).to have_current_path('/baby_names?starts_with=A&ends_with=&commit=Générer')
+    expect(page).to have_content 'A : 228'
+  end
+
+  it 'Check path with ends_with' do
+    visit root_path
+    fill_in 'starts_with', with: ''
+    fill_in 'ends_with', with: 'a'
+    click_button 'Générer'
+    expect(page).to have_current_path('/baby_names?starts_with=&ends_with=a&commit=Générer')
+    expect(page).to have_content 'A : 73'
+  end
+end
+
+describe 'Test button section about' do
+  include Capybara::DSL
+
+  it 'Check that the button about exists' do
+    visit root_path
+    expect(page).to have_selector(:css, 'a.btn.btn-xl.btn-outline-dark[href="https://pastebin.com/NkQrnugq"]',
+                                  text: "Voir l'algorithme")
+  end
+end
+
+describe 'Test footer links' do
+  include Capybara::DSL
+
+  it 'Check that the footer links exist' do
+    visit root_path
+    expect(page).to have_selector(:css, 'a[href="https://github.com/hgueguen"]', text: 'Harry JMG')
+    expect(page).to have_selector(:css, 'a[href="https://github.com/Symitsh"]')
+    expect(page).to have_selector(:css, 'a[href="https://github.com/Lauris-Bessone"]')
+    expect(page).to have_selector(:css, 'a[href="https://getbootstrap.com/"]', text: 'Bootstrap')
+    expect(page).to have_selector(:css, 'a[href="https://github.com/Symitsh/Baby_names_generator"]',
+                                  text: 'BABY NAME GENERATOR')
   end
 end
