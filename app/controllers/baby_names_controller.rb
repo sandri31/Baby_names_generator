3167548466e4fc077ea05c17b09db03290e2ab46
Symitsh <sandri822@hotmail.fr>
2023-01-04 10:58:14 +0100

    🚧 Add in list name

--- app/controllers/baby_names_controller.rb
+++ app/controllers/baby_names_controller.rb
@@ -9,14 +9,21 @@ class BabyNamesController < ApplicationController
   include BabyNameGenerator
 
   def baby_names
-    required_chars = params[:required_chars].downcase || 'na' # If no value is entered, use 'na' by default
-    starts_with = params[:starts_with].downcase
-    ends_with = params[:ends_with].downcase
+    required_chars = params.fetch(:required_chars, 'na').downcase # If no value is entered, use 'na' by default
+    starts_with = params.fetch(:starts_with, '').downcase
+    ends_with = params.fetch(:ends_with, '').downcase
 
     starts_with = params.fetch(:starts_with, '').downcase
     ends_with = params.fetch(:ends_with, '').downcase
 
     BabyNameGenerator.instance_variable_set(:@required_global, required_chars.split(' '))
     @names = BabyNameGenerator.generate_baby_generator(starts_with: starts_with, ends_with: ends_with).to_a
+
+    # Récupérez le prénom soumis dans le formulaire
+    prenom = params[:prenom]
+
+    # Ajoutez le prénom à votre liste des prénoms
+    @prenoms ||= []
+    @prenoms << prenom
   end
 end
